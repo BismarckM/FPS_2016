5e18b3aa82bff5887002b59a27c4d2da4d846468
YangWoomin <cicyowoo@naver.com>
2016-07-04 20:33:32 +0900

    commit1

---
+++ Test_FPS_Project/Assets/AngryBotPlayer/PlayerCtl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerCtl : MonoBehaviour {
+    public Animation _animation;
+    public Anim anim;
+    
+	// Use this for initialization
+	void Start () {
+        _animation = GetComponentInChildren<Animation>();
+        _animation.clip = anim.idle;
+        _animation.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
