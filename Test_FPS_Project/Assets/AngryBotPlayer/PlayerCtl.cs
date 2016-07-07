2ad0a42b279775be1137336fd8e83b2c3328deec
YangWoomin <cicyowoo@naver.com>
2016-07-07 21:26:45 +0900

    commit3

--- Test_FPS_Project/Assets/AngryBotPlayer/PlayerCtl.cs
+++ Test_FPS_Project/Assets/AngryBotPlayer/PlayerCtl.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Anim
+{
+    public AnimationClip idle;
+    public AnimationClip runFoward;
+    public AnimationClip runBackward;
+    public AnimationClip runRight;
+    public AnimationClip runLeft;
+}
 
 public class PlayerCtl : MonoBehaviour {
+    private Transform tr;
+    private float moveSpeed = 5.0f;
+    private float rotSpeed = 1.2f;
+    private Vector3 moveDir;
     public Animation _animation;
     public Anim anim;
-    
-	// Use this for initialization
-	void Start () {
+
+    private float beforePosition;
+
+    // Use this for initialization
+    void Start () {
+        tr = GetComponent<Transform>();
         _animation = GetComponentInChildren<Animation>();
         _animation.clip = anim.idle;
         _animation.Play();
@@ -15,6 +31,106 @@ public class PlayerCtl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        //Touch touch = Input.GetTouch(0);
+        //if (touch.phase == TouchPhase.Began)
+        //{
+        //    beforePosition = touch.position.x;
+        //}
+        //else
+        //{
+        //    tr.Rotate(Time.deltaTime * 3.0f * new Vector3(0, beforePosition - touch.position.x, 0));
+        //    beforePosition = touch.position.x;
+        //}
+
+        float accelX = Input.acceleration.x;
+        float accelY = -Input.acceleration.y;
+
+        // character movement for left and right
+        if(accelX < -0.8f) {
+            accelX = 0.0f;
+        }
+        else if(accelX < -0.15f)
+        {
+           
+        }
+        else if(accelX < 0.15f)
+        {
+            accelX = 0.0f;
+        }
+        else if(accelX < 0.8f)
+        {
+
+        }
+        else
+        {
+            accelX = 0.0f;
+        }
+
+        // character movement for forward and backward
+        if (accelY < 0.0f)
+        {
+            accelY = 0.0f;
+        }
+        else if (accelY < 0.65f)
+        {
+           
+        }
+        else if (accelY < 0.85f)
+        {
+            accelY = 0.0f;
+        }
+        else if (accelY < 1.0f)
+        {
+            accelY = -accelY + 0.5f;
+        }
+        else
+        {
+            accelY = 0.0f;
+        }
+
+        moveDir = (new Vector3(accelX, 0, accelY)).normalized;
+        tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
+
+        // for animation
+        if (moveDir.x > 0.0f && moveDir.z >= 0.0f && moveDir.x > moveDir.z)
+        {
+            _animation.CrossFade(anim.runRight.name, 0.1f);
+        }
+        else if (moveDir.x >= 0.0f && moveDir.z > 0.0f && moveDir.x <= moveDir.z)
+        {
+            _animation.CrossFade(anim.runFoward.name, 0.1f);
+        }
+        else if (moveDir.x > 0.0f && moveDir.z <= 0.0f && moveDir.x > -moveDir.z)
+        {
+            _animation.CrossFade(anim.runRight.name, 0.1f);
+        }
+        else if (moveDir.x >= 0.0f && moveDir.z < 0.0f && moveDir.x <= -moveDir.z)
+        {
+            _animation.CrossFade(anim.runBackward.name, 0.1f);
+        }
+        else if (moveDir.x < 0.0f && moveDir.z >= 0.0f && -moveDir.x > moveDir.z)
+        {
+            _animation.CrossFade(anim.runLeft.name, 0.1f);
+        }
+        else if (moveDir.x <= 0.0f && moveDir.z > 0.0f && -moveDir.x <= moveDir.z)
+        {
+            _animation.CrossFade(anim.runFoward.name, 0.1f);
+        }
+        else if (moveDir.x < 0.0f && moveDir.z <= 0.0f && -moveDir.x > -moveDir.z)
+        {
+            _animation.CrossFade(anim.runLeft.name, 0.1f);
+        }
+        else if (moveDir.x <= 0.0f && moveDir.z < 0.0f && -moveDir.x <= -moveDir.z)
+        {
+            _animation.CrossFade(anim.runBackward.name, 0.1f);
+        }
+        else if (moveDir.x == 0.0f && moveDir.z == 0.0f)
+        {
+            _animation.CrossFade(anim.idle.name, 0.1f);
+        }
+        else
+        {
+            _animation.CrossFade(anim.idle.name, 0.1f);
+        }
+    }
 }
--- Test_FPS_Project/Assets/Controller.cs
+++ Test_FPS_Project/Assets/Controller.cs
@@ -2,26 +2,27 @@
 using System.Collections;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(AudioSource))]
-[System.Serializable]
-public class Anim
-{
-    public AnimationClip idle;
-    public AnimationClip runFoward;
-    public AnimationClip runBackward;
-    public AnimationClip runRight;
-    public AnimationClip runLeft;
-}
+//[RequireComponent(typeof(AudioSource))]
+//[System.Serializable]
+//public class Anim
+//{
+//    public AnimationClip idle;
+//    public AnimationClip runFoward;
+//    public AnimationClip runBackward;
+//    public AnimationClip runRight;
+//    public AnimationClip runLeft;
+//}
 
 public class Controller : MonoBehaviour {
     //public Image Stick;
     //private Vector3 originPos = Vector3.zero;
     //float StickRadius = 0.0f;
+
     private float beginPosition;
     private float currentPosition;
 
     public Image fireButton;
-    public GameObject bullet;
+    //public GameObject bullet;
     public Transform firePos;
 
     public MeshRenderer muzzleFlash;
@@ -30,32 +31,32 @@ public class Controller : MonoBehaviour {
     private AudioSource source = null;
 
     public Transform tr;
-    private float moveSpeed = 5.0f;
-    private float rotSpeed = 1.2f;
-    private Vector3 moveDir;
-    public Anim anim;
-    public Animation _animation;
+    //private float moveSpeed = 5.0f;
+    private float rotSpeed = 3.0f;
+    //private Vector3 moveDir;
+    //public Anim anim;
+    //public Animation _animation;
 
-    public Vector3 forceVec;
-    public Rigidbody rb;
+    //public Vector3 forceVec;
+    //public Rigidbody rb;
 
     // Use this for initialization
     void Start () {
         // fire effect
         muzzleFlash.enabled = false;
-
+        
         // for joystick
-        //originPos = Stick.transform.position;
-        //StickRadius = Stick.rectTransform.sizeDelta.x / 2;
+        ////originPos = Stick.transform.position;
+        ////StickRadius = Stick.rectTransform.sizeDelta.x / 2;
 
-        // for animation
-        _animation = tr.GetComponentInChildren<Animation>();
-        _animation.clip = anim.idle;
-        _animation.Play();
+        //// for animation
+        //_animation = tr.GetComponentInChildren<Animation>();
+        //_animation.clip = anim.idle;
+        //_animation.Play();
 
-        // for gyroscope sensor
-        Input.gyro.enabled = true;
-        rb = GetComponent<Rigidbody>();
+        //// for gyroscope sensor
+        //Input.gyro.enabled = true;
+        //rb = GetComponent<Rigidbody>();
 
         // for sound
         source = GetComponent<AudioSource>();
@@ -79,7 +80,7 @@ public class Controller : MonoBehaviour {
 
     void CreateBullet()
     {
-        Instantiate(bullet, firePos.position, firePos.rotation);
+        // Instantiate(bullet, firePos.position, firePos.rotation);
     }
 
     IEnumerator ShowMuzzleFlash()
@@ -104,14 +105,16 @@ public class Controller : MonoBehaviour {
         Touch touch = Input.GetTouch(0);
         Vector3 rotDir = Vector3.zero;
         rotDir.y = beginPosition - touch.position.x;
-        
-        //if(rotDir.sqrMagnitude > 1)
+
+        //if (rotDir.sqrMagnitude > 1)
         //{
         //    rotDir.Normalize();
         //}
         tr.Rotate(rotDir * Time.deltaTime * rotSpeed);
+        // tr.Rotate(Time.deltaTime * rotSpeed * Vector3.up);
+        // tr.RotateAround(tr.transform.position, Vector3.up, beginPosition - touch.position.x);
         beginPosition = touch.position.x;
-        Debug.Log(beginPosition + ", " + rotDir);
+        //Debug.Log(beginPosition + ", " + rotDir);
     }
 
     //public void OnDrag()
@@ -146,44 +149,44 @@ public class Controller : MonoBehaviour {
 
     void Update()
     {
-        // 2016-5-30 using acceleration for movement
-        float accelX = Input.acceleration.x;
-        float accelY = -Input.acceleration.y;
+        //// 2016-5-30 using acceleration for movement
+        //float accelX = Input.acceleration.x;
+        //float accelY = -Input.acceleration.y;
 
-        // character movement for left and right
-        if(accelX > -0.15f && accelX < 0.15f)
-        {
-            accelX = 0.0f;
-        }
-        else if(accelX > 0.8f || accelX < -0.8f)
-        {
-            accelX = 0.0f;
-        }
+        //// character movement for left and right
+        //if(accelX > -0.15f && accelX < 0.15f)
+        //{
+        //    accelX = 0.0f;
+        //}
+        //else if(accelX > 0.8f || accelX < -0.8f)
+        //{
+        //    accelX = 0.0f;
+        //}
 
-        // character movement for forward and backward
-        if(accelY < 0.0f)
-        {
-            accelY = 0.0f;
-        }
-        else if(accelY < 0.65f)
-        {
-            // maintain the value
-        }
-        else if(accelY < 0.85f)
-        {
-            accelY = 0.0f;
-        }
-        else if(accelY < 1.0f)
-        {
-            accelY = -accelY + 0.5f;
-        }
-        else
-        {
-            accelY = 0.0f;
-        }
+        //// character movement for forward and backward
+        //if(accelY < 0.0f)
+        //{
+        //    accelY = 0.0f;
+        //}
+        //else if(accelY < 0.65f)
+        //{
 
-        moveDir = (new Vector3(accelX, 0, accelY)).normalized;
-        tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
+        //}
+        //else if(accelY < 0.85f)
+        //{
+        //    accelY = 0.0f;
+        //}
+        //else if(accelY < 1.0f)
+        //{
+        //    accelY = -accelY + 0.5f;
+        //}
+        //else
+        //{
+        //    accelY = 0.0f;
+        //}
+
+        //moveDir = (new Vector3(accelX, 0, accelY)).normalized;
+        //tr.Translate(moveDir * Time.deltaTime * moveSpeed, Space.Self);
         // Debug.Log(moveDir);
 
         //Touch touch = Input.GetTouch(0);
@@ -207,45 +210,45 @@ public class Controller : MonoBehaviour {
         //// tr.Rotate(dir * Time.deltaTime * rotSpeed);
 
         // for animation
-        if (moveDir.x > 0.0f && moveDir.z >= 0.0f && moveDir.x > moveDir.z)
-        {
-            _animation.CrossFade(anim.runRight.name, 0.1f);
-        }
-        else if (moveDir.x >= 0.0f && moveDir.z > 0.0f && moveDir.x <= moveDir.z)
-        {
-            _animation.CrossFade(anim.runFoward.name, 0.1f);
-        }
-        else if (moveDir.x > 0.0f && moveDir.z <= 0.0f && moveDir.x > -moveDir.z)
-        {
-            _animation.CrossFade(anim.runRight.name, 0.1f);
-        }
-        else if (moveDir.x >= 0.0f && moveDir.z < 0.0f && moveDir.x <= -moveDir.z)
-        {
-            _animation.CrossFade(anim.runBackward.name, 0.1f);
-        }
-        else if (moveDir.x < 0.0f && moveDir.z >= 0.0f && -moveDir.x > moveDir.z)
-        {
-            _animation.CrossFade(anim.runLeft.name, 0.1f);
-        }
-        else if (moveDir.x <= 0.0f && moveDir.z > 0.0f && -moveDir.x <= moveDir.z)
-        {
-            _animation.CrossFade(anim.runFoward.name, 0.1f);
-        }
-        else if (moveDir.x < 0.0f && moveDir.z <= 0.0f && -moveDir.x > -moveDir.z)
-        {
-            _animation.CrossFade(anim.runLeft.name, 0.1f);
-        }
-        else if (moveDir.x <= 0.0f && moveDir.z < 0.0f && -moveDir.x <= -moveDir.z)
-        {
-            _animation.CrossFade(anim.runBackward.name, 0.1f);
-        }
-        else if (moveDir.x == 0.0f && moveDir.z == 0.0f)
-        {
-            _animation.CrossFade(anim.idle.name, 0.1f);
-        }
-        else
-        {
-            _animation.CrossFade(anim.idle.name, 0.1f);
-        }
+        //if (moveDir.x > 0.0f && moveDir.z >= 0.0f && moveDir.x > moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runRight.name, 0.1f);
+        //}
+        //else if (moveDir.x >= 0.0f && moveDir.z > 0.0f && moveDir.x <= moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runFoward.name, 0.1f);
+        //}
+        //else if (moveDir.x > 0.0f && moveDir.z <= 0.0f && moveDir.x > -moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runRight.name, 0.1f);
+        //}
+        //else if (moveDir.x >= 0.0f && moveDir.z < 0.0f && moveDir.x <= -moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runBackward.name, 0.1f);
+        //}
+        //else if (moveDir.x < 0.0f && moveDir.z >= 0.0f && -moveDir.x > moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runLeft.name, 0.1f);
+        //}
+        //else if (moveDir.x <= 0.0f && moveDir.z > 0.0f && -moveDir.x <= moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runFoward.name, 0.1f);
+        //}
+        //else if (moveDir.x < 0.0f && moveDir.z <= 0.0f && -moveDir.x > -moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runLeft.name, 0.1f);
+        //}
+        //else if (moveDir.x <= 0.0f && moveDir.z < 0.0f && -moveDir.x <= -moveDir.z)
+        //{
+        //    _animation.CrossFade(anim.runBackward.name, 0.1f);
+        //}
+        //else if (moveDir.x == 0.0f && moveDir.z == 0.0f)
+        //{
+        //    _animation.CrossFade(anim.idle.name, 0.1f);
+        //}
+        //else
+        //{
+        //    _animation.CrossFade(anim.idle.name, 0.1f);
+        //}
     }
 }
